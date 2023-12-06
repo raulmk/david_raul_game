9e7dd8a6519eeb58840c768d724059bedc8c64df
David Moreno Fernández <144929862+DavidMorenoF@users.noreply.github.com>
2023-12-06 11:34:58 +0100

    menu

--- images.g.ts
+++ images.g.ts
@@ -346,19 +346,38 @@ ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 . . . . . . . . . . . . . . . . 
 `;
             case "image9":
-            case "myImage3":return img`
+            case "help":return img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
+f f f f f f f f f f f f f f f f 
+f 1 f 1 f 1 1 1 f 1 f f 1 1 f f 
+f 1 f 1 f 1 f f f 1 f f 1 f 1 f 
+f 1 f 1 f 1 f f f 1 f f 1 f 1 f 
+f 1 1 1 f 1 1 f f 1 f f 1 f 1 f 
+f 1 f 1 f 1 f f f 1 f f 1 1 f f 
+f 1 f 1 f 1 1 1 f 1 1 f 1 f f f 
+f f f f f f f f f f f f f f f f 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
+`;
+            case "image10":
+            case "play":return img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
+f f f f f f f f f f f f f f f f 
+f 2 2 f f 2 f f f 2 f f 2 f 2 f 
+f 2 f 2 f 2 f f 2 f 2 f 2 2 2 f 
+f 2 f 2 f 2 f f 2 f 2 f f 2 f f 
+f 2 f 2 f 2 f f 2 2 2 f f 2 f f 
+f 2 2 f f 2 f f 2 f 2 f f 2 f f 
+f 2 f f f 2 2 f 2 f 2 f f 2 f f 
+f f f f f f f f f f f f f f f f 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
